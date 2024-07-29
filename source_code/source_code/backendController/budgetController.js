5a3f39b08c81ad16bde1e2b5d3de033f46c83e44
arafimam <arafimam@student.ubc.ca>
2024-07-29 14:59:36 -0700

    Backend services for create, edit and delete budgets

--- source_code/source_code/backendController/budgetController.js
+++ source_code/source_code/backendController/budgetController.js
@@ -17,5 +17,63 @@ router.get("/getAllBudgets", async(req, res) => {
             message: "Failed to retrieve all budgets."
         })
     }
-})
+});
+
+router.post("/createBudget", async(req, res) => {
+    const startDate = req.body.startDate;
+    const endDate = req.body.endDate;
+    const amount = req.body.amount;
+    const managerID = req.body.managerID;
+    const projectType = req.body.projectType;
+
+    const isBudgetCreated = await budgetService.createBudget(startDate, endDate, amount, managerID, projectType);
+    if(isBudgetCreated.success === true) {
+        res.status(200).json({
+            message: "Successfully created budget"
+        })
+    }
+    else {
+        res.status(500).json({
+            message: "Unable to create budget."
+        })
+    }
+});
+
+router.post("/editBudget", async (req, res) => {
+    const startDate = req.body.startDate;
+    const endDate = req.body.endDate;
+    const amount = req.body.amount;
+    const managerID = req.body.managerID;
+    const projectType = req.body.projectType;
+    const budgetID = req.body.budgetID;
+
+    const isBudgetUpdated = await budgetService.updateBudget(budgetID, startDate, endDate, amount, managerID, projectType);
+    if(isBudgetUpdated.success === true) {
+        res.status(200).json({
+            message: "Successfully update budget"
+        })
+    }
+    else {
+        res.status(500).json({
+            message: "Unable to update budget."
+        })
+    }
+});
+
+router.post("/deleteBudget", async (req, res) => {
+    const budgetID = req.body.budgetID;
+
+    const isBudgetDeleted = await budgetService.deleteBudget(budgetID);
+    if(isBudgetDeleted.success === true) {
+        res.status(200).json({
+            message: "Successfully deleted budget"
+        })
+    }
+    else {
+        res.status(500).json({
+            message: "Unable to delete budget."
+        })
+    }
+});
+
 module.exports = router;
--- source_code/source_code/backendServices/budgetService.js
+++ source_code/source_code/backendServices/budgetService.js
@@ -26,6 +26,80 @@ async function getAllBudgetInformation() {
         return {success: false};
     })
 }
+
+async function createBudget(startDate, endDate, amount, managerID, projectType) {
+    return await appService.withOracleDB(async (connection) => {
+        const budgetID = appService.generateUserID();
+        const budgetInsertionResult = await connection.execute(
+            `
+            INSERT INTO Budget(budgetID, startDate, endDate, amount, managerID, projectType) 
+            VALUES (budgetID, :startDate, :endDate, :amount ,:managerID, :projectType)
+            `,
+            {budgetID, startDate, endDate, amount, managerID, projectType},
+            {autocommit: true}
+        );
+
+        if (budgetInsertionResult.rowsAffected && budgetInsertionResult.rowsAffected > 0) {
+            return {success: true };
+        };
+    }).catch((error) => {
+        console.error('Error in withOracleDB: ',error);
+        return {success: false};
+    })
+}
+
+async function updateBudget(budgetID, startDate, endDate, amount, managerID, projectType) {
+    return await appService.withOracleDB(async (connection) => {
+        const updateResult = await connection.execute(
+            `
+            UPDATE Budget SET startDate = :startDate, 
+            endDate = :endDate, 
+            amount = :amount, 
+            managerID = :managerID, 
+            projectType = :projectType
+            WHERE budgetID = :budgetID
+            `,
+            {startDate, endDate, amount, managerID, projectType, budgetID},
+            {autoCommit: true}
+        );
+
+        if (updateResult.rowsAffected && updateResult.rowsAffected > 0) {
+            return {success: true};
+        } else {
+            return {success: false };
+        }
+    }).catch((error) => {
+        console.error('Error in withOracleDB:', error);
+        return {success: false };
+    });
+}
+
+async function deleteBudget(budgetID) {
+    return await appService.withOracleDB(async (connection) => {
+        const deleteResult = await connection.execute(
+            `
+            DELETE FROM Budget WHERE budgetID = :budgetID
+            `,
+            {budgetID},
+            {autoCommit: true}
+        );
+
+        if (deleteResult.rowsAffected && deleteResult.rowsAffected > 0) {
+            return {success: true};
+        } else {
+            return {success: false};
+        }
+    }).catch((error) => {
+        console.error('Error in withOracleDB during delete: ', error);
+        return {success: false, error: error.message};
+    });
+}
+
+
+
 module.exports = {
     getAllBudgetInformation,
+    createBudget,
+    updateBudget,
+    deleteBudget
 }
